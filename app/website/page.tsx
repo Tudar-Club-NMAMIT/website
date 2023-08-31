2a25f9ba5e634c5f3a0c09f42d186d7f14ca9465
Htivus <suvithkumar0001@gmail.com>
2023-08-31 19:33:27 +0530

    footer

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 const page = () => {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </div>
   )
 }
 
