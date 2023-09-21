5b744148e88ad3c78f618eedf8e73b0ab72963ed
Htivus <suvithkumar0001@gmail.com>
2023-09-21 14:04:21 +0530

    components folder

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Hero from '../components/Hero'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 const page = () => {
   return (
