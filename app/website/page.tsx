123a75437b9cb99d5fe7b1caa315418c068400af
Htivus <suvithkumar0001@gmail.com>
2023-11-11 17:46:50 +0530

    added documentary section

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import Documentation from './components/Documentation'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Documentation/>
       <Footer/>
     </div>
   )
 }
 
-export default page
+export default page;
