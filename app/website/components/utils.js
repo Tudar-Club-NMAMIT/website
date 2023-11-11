57e8f50e4456746e1d842b13f1da79e9d2741711
Amith-Jagannath <amithjagannath02@gmail.com>
2023-11-11 17:48:56 +0530

    added gallery section

---
+++ app/website/components/utils.js
@@ -0,0 +1,12 @@
+// Preload images
+const preloadImages = (selector = "img") => {
+  return new Promise((resolve) => {
+    imagesLoaded(
+      document.querySelectorAll(selector),
+      { background: true },
+      resolve
+    );
+  });
+};
+
+export { preloadImages };
