29a2e9988f11b21dd90632a5adbc8bad8446bd89
Prathik-2002 <acharyaprathik4@gmail.com>
2023-11-13 11:21:27 +0530

    conflict removed

--- app/website/components/Navbar.tsx
+++ app/website/components/Navbar.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./navbar.css";
-
 import Link from "next/link";
-
 const NavLinks = [
   { href: "/", key: "Home", text: "Home" },
   { href: "/", key: "Club", text: "Club" },
@@ -12,27 +10,25 @@ const NavLinks = [
 ];
 const Navbar = () => {
   return (
-    <nav className=" flexBetween py-5 px-8 border-b border-nav-border gap-4">
-      <div className="flex w-full ">
+    <nav className="py-5 px-8 border-b border-nav-border">
+      <div className="flex w-full">
         <a href="#" className="float-left ml-0">
-          <span className=" text-2xl font-semibold whitespace-nowrap ">
-            Tudar
-          </span>
+          <span className="text-2xl whitespace-nowrap ">Tudar</span>
         </a>
-
         {/* <p className="text-bold text-3xl m-4">Tudar</p> */}
         <div className="flex items-center justify-center w-screen ">
-          <ul className="lg:flex justify-center hidden min-w-screen text-sm gap-10 margin-auto">
-            {NavLinks.map((link) => (
-              <Link
-                className="font-extrabold alink"
-                href={link.href}
-                key={link.key}
-              >
-                {link.text}
-              </Link>
-            ))}
-          </ul>
+        <ul className="md:flex justify-center hidden min-w-screen text-sm gap-10 margin-auto navlist">
+          {NavLinks.map((link) => (
+            <Link className="font-extrabold alink" href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+        </div>
+        <div className="md:hidden flex  w-10 h float-right m-0 hamburger p-1">
+          <div className="bar-hamburger"></div>
+          <div className="bar-hamburger"></div>
+          <div className="bar-hamburger"></div>
         </div>
       </div>
       <div className="flexCenter"></div>
@@ -40,5 +36,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
