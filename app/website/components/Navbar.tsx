d1efd3153889c3f1d7bb9cc2795ebd49ff53578c
Prathik-2002 <acharyaprathik4@gmail.com>
2023-11-13 10:31:03 +0530

    Navbar hamburger

--- app/website/components/Navbar.tsx
+++ app/website/components/Navbar.tsx
@@ -15,17 +15,16 @@ const NavLinks = [
 const Navbar = () => {
   
   return (
-    <nav className=" flexBetween py-5 px-8 border-b border-nav-border gap-4">
-      <div
-        className="flex w-full "
-      >
+    <nav className="py-5 px-8 border-b border-nav-border">
+      <div className="flex w-full">
         <a href="#" className="float-left ml-0">
-        <span className=" text-2xl font-semibold whitespace-nowrap ">Tudar</span>
+        <span className="text-2xl whitespace-nowrap ">Tudar</span>
         </a>
+
         {/* <p className="text-bold text-3xl m-4">Tudar</p> */}
         <div className="flex items-center justify-center w-screen ">
 
-        <ul className="lg:flex justify-center hidden min-w-screen text-sm gap-10 margin-auto">
+        <ul className="md:flex justify-center hidden min-w-screen text-sm gap-10 margin-auto navlist">
           {NavLinks.map((link) => (
             <Link className="font-extrabold alink" href={link.href} key={link.key}>
               {link.text}
@@ -33,7 +32,12 @@ const Navbar = () => {
           ))}
         </ul>
         </div>
-
+        <div className="md:hidden flex  w-10 h float-right m-0 hamburger p-1">
+          <div className="bar-hamburger"></div>
+          <div className="bar-hamburger"></div>
+          <div className="bar-hamburger"></div>
+        </div>
+        
       </div>
       <div className="flexCenter">
        </div>
