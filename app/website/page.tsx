0b865619c4d85adc4c93174541b1350a8cdf1909
Htivus <suvithkumar0001@gmail.com>
2023-11-13 13:49:52 +0530

    removed hero section repeat

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Documentation from "./components/Documentation";
 import { Gallery } from "./components/Gallery";
 
@@ -9,7 +8,6 @@ const page = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
       <Gallery />
       <Documentation />
       <Footer />
