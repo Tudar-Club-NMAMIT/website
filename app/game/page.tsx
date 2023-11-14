d2dd37bf278e9405e61ef51ca572a4c9df69ee34
Htivus <suvithkumar0001@gmail.com>
2023-11-14 11:55:23 +0530

    error rm

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Keyboard from './components/Jumbled'
 import Matchboard from './components/Matched'
-import Wordtobematched from './components/wordtobematched'
 import Navbar from './components/Navbar'
 
 const page = () => {
   return (
     <>
     <Navbar />
-    <Wordtobematched/>
     <Matchboard/>
     <Keyboard/>
     </>
