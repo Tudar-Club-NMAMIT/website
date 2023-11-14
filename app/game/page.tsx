5820af944331a163b0655035287b037fc29df34b
Htivus <suvithkumar0001@gmail.com>
2023-11-14 11:53:08 +0530

    test

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Keyboard from './components/keyboard'
-import Matchboard from './components/matchboard'
+import Keyboard from './components/Jumbled'
+import Matchboard from './components/Matched'
 import Wordtobematched from './components/wordtobematched'
 import Navbar from './components/Navbar'
 
 const page = () => {
   return (
     <>
-    <Navbar/>
+    <Navbar />
     <Wordtobematched/>
     <Matchboard/>
     <Keyboard/>
