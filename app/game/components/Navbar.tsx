c1702f29fbb0ec8dc839600dc914834c84e65406
Prathik-2002 <acharyaprathik4@gmail.com>
2023-11-14 12:37:51 +0530

    GAme nav

--- app/game/components/Navbar.tsx
+++ app/game/components/Navbar.tsx
@@ -1,33 +1,25 @@
 import React from "react";
 import "./navbar.css";
 import Link from "next/link";
-const NavLinks = [
-  { href: "/", key: "Back", text: "Back" },
-  { href: "/", key: "Home", text: "Home" },
-  { href: "/", key: "About", text: "About" },
-];
+import { HelpCircle, Menu, Flame } from 'lucide-react';
+
+
 const Navbar = () => {
   return (
-    <nav className="py-5 px-8 border-b border-nav-border bg-black">
+    <nav className="py-2 px-8 border-b border-nav-border bg-white">
       <div className="flex w-full">
-        <a href="#" className="float-left ml-0">
-          <span className="text-2xl whitespace-nowrap "><strong>TULIPU</strong></span>
-        </a>
+        <Menu className=" h-10 float-left p-0"/>      
         {/* <p className="text-bold text-3xl m-4">Tudar</p> */}
         <div className="flex items-center justify-center w-screen ">
         <ul className="md:flex justify-center hidden min-w-screen text-sm gap-10 margin-auto navlist">
-          {NavLinks.map((link) => (
-            <Link className="font-extrabold alink" href={link.href} key={link.key}>
-              {link.text}
-            </Link>
-          ))}
+          
+          <span className="extra-bold alink">Tudar-Tulipu</span>
         </ul>
         </div>
-        <div className="md:hidden flex  w-10 h float-right m-0 hamburger p-1">
-          <div className="bar-hamburger"></div>
-          <div className="bar-hamburger"></div>
-          <div className="bar-hamburger"></div>
-        </div>
+       
+        <HelpCircle className="h-10 float-right mr-5"/>
+        <Flame className="h-10 float-right "/>
+
       </div>
       <div className="flexCenter"></div>
       <div className="fixed  bg-gray-200 bottom-10 right-10 rounded-full w-10 h-10"></div>
