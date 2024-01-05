bccc1c54ad33e819d347955ca4be4b63387f1338
Adarshhetty <adarshetty18@gmail.com>
2024-01-05 11:09:13 +0530

    rebuilt-website

---
+++ app/website/components/Events.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+         <div className="h-screen bg-gray-900 w-full">
+
+         </div>
+      
+    </div>
+  )
+}
+
+export default Events
