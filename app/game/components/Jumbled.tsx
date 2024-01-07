94babebabb717999c3fdfc73cce0df7ca8d0e8c4
Htivus <suvithkumar0001@gmail.com>
2024-01-07 13:12:31 +0530

    deploy01

--- app/game/components/Jumbled.tsx
+++ app/game/components/Jumbled.tsx
@@ -1,68 +1,9 @@
-const Keyboard = () => {
-    return ( 
-        <>
-        <div className="container mx-auto p-4">
-        <div className="keyboard bg-gray-200 p-4 rounded-lg">
-        {/* Row 1 */}
-        <div className="flex">
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2 p-2 bg-white rounded-lg">Backspace</div>
-        </div>
-        {/*  row 2 */}
-        <div className="flex">
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2 p-2 bg-white rounded-lg">Backspace</div>
-        </div>
-        {/* row 3 */}
-        <div className="flex">
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2 p-2 bg-white rounded-lg">Backspace</div>
-        </div>
-        {/* row 4 */}
-        <div className="flex">
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg ">space bar</div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2  bg-white rounded-lg w-1/12"><img src="img/1.jpg" alt="" /></div>
-          <div className="key m-2 p-2 bg-white rounded-lg">Backspace</div>
-        </div>
-      </div>
-    </div>
-        </>
-     );
+import React from 'react'
+
+const Jumbled = () => {
+  return (
+    <div>Jumbled</div>
+  )
 }
- 
-export default Keyboard;
+
+export default Jumbled
