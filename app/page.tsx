13e9ec502da83faaf7f8ae6eba7d3d2613ee6aa3
Adarshhetty <adarshetty18@gmail.com>
2024-01-11 10:26:45 +0530

    About section mask effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "./website/components/Navbar/Navbar";
 import Club from "./website/components/Club/Club";
 import Hero from "./website/components/Hero/Hero";
-import StickyCursor from "./website/components/StickyCursor/StickyCursor";
 import Events from "./website/components/Main-Events/Events";
 import Card from "./website/components/Card-section/Card";
 import Blog from "./website/components/Blog Carousel/Blog";
@@ -11,7 +10,6 @@ import Footer from "./website/components/Footer/Footer";
 export default function Home() {
   return (
     <div className="bg-gray-900">
-      <StickyCursor />
       <Navbar />
       <Hero />
       <Club />
--- app/website/components/Club/Club.tsx
+++ app/website/components/Club/Club.tsx
@@ -1,22 +1,48 @@
 "use client";
 import Image from "next/image";
-
+import { MaskContainer } from "../ui/svg-mask-effect";
 import "./Club.css";
 export default function Club() {
   return (
-    <div className="flex pt-12 px-8 md:px-8  items-center justify-center bg-hero md:h-screen overflow-hidden">
+    // <div className="flex pt-12 px-8 md:px-8  items-center justify-center bg-hero md:h-screen overflow-hidden">
+    //   <div className="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
+    //     <div className="w-full md:w-1/2 lg:pr-10">
+    //       <h1 className=" m-6 font-bold club-heading text-3xl lg:text-5xl text-left text-[#afa18f] ">
+    //         Bridging Cultures, Celebrating Tulu Heritage
+    //       </h1>
+    //       <h3 className="m-6  md:mt-10 text-md lg:text-xl text-justify  text-white opacity-50  ">
+    //         At TUDAR Club, we are on a mission to celebrate, preserve, and
+    //         propagate the vibrant heritage of the Tulu language. Rooted in the
+    //         cultural mosaic of coastal Karnataka, Tulu is a language that
+    //         encapsulates centuries of tradition, folklore, and a unique
+    //         identity.
+    //       </h3>
+    //     </div>
+    //     <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+    //       <Image width={250} height={300} src="/src/logo.png" alt="" />
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="flex pt-12 px-14  items-center justify-center bg-hero md:h-screen overflow-hidden">
       <div className="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
         <div className="w-full md:w-1/2 lg:pr-10">
-          <h1 className=" m-6 font-bold club-heading text-3xl lg:text-5xl text-left text-[#afa18f] ">
-            Bridging Cultures, Celebrating Tulu Heritage
-          </h1>
-          <h3 className="m-6  md:mt-10 text-md lg:text-xl text-justify  text-white opacity-50  ">
-            At TUDAR Club, we are on a mission to celebrate, preserve, and
+          <MaskContainer
+            revealText={
+              <p className="  text-white text-justify font-bold text-2xl">
+                At TUDAR Club, we are on a mission to celebrate, preserve, and
+                propagate the vibrant heritage of the Tulu language. Rooted in
+                the cultural mosaic of coastal Karnataka, Tulu is a language
+                that encapsulates centuries of tradition, folklore, and a unique
+                identity.
+              </p>
+            }
+            className="h-[40rem] ">
+            Materegla solmelu to celebrate, preserve, and
             propagate the vibrant heritage of the Tulu language. Rooted in the
             cultural mosaic of coastal Karnataka, Tulu is a language that
             encapsulates centuries of tradition, folklore, and a unique
             identity.
-          </h3>
+          </MaskContainer>
         </div>
         <div className="w-full md:w-1/2 flex justify-center md:justify-end">
           <Image width={250} height={300} src="/src/logo.png" alt="" />
