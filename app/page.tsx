65f5b65e0f359bce5482ca52270f04ecb183ad28
Htivus <suvithkumar0001@gmail.com>
2024-01-13 20:10:35 +0530

    Prisma added and Next 14

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,24 @@ import Events from "./website/components/Main-Events/Events";
 import Card from "./website/components/Card-section/Card";
 import Blog from "./website/components/Blog Carousel/Blog";
 import Footer from "./website/components/Footer/Footer";
+import { prisma } from "./utils/db";
+import { uploadName } from "./server/actions";
+import Testform from "./website/components/Testform";
 
 
-export default function Home() {
+ export default async function Home() {
+  const data = await prisma.test.findFirst()
   return (
     <div className="bg-gray-900">
-      <Navbar />
+      {data?.name}
+      <Testform/>
+      {/* <Navbar />
       <Hero />
       <Club />
       <Events />
       <Card/>
       <Blog/>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
