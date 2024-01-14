2be5c8f0a3593a350c54e864f531f9b4fbd2cc2f
Adarshhetty <adarshetty18@gmail.com>
2024-01-14 11:31:44 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from "./website/components/Navbar/Navbar";
 import Club from "./website/components/Club/Club";
 import Hero from "./website/components/Hero/Hero";
@@ -5,13 +6,18 @@ import Events from "./website/components/Main-Events/Events";
 import Card from "./website/components/Card-section/Card";
 import Blog from "./website/components/Blog Carousel/Blog";
 import Footer from "./website/components/Footer/Footer";
+import Video from "./website/components/Video-section/Video";
+import { useSession } from "next-auth/react";
 
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  
   return (
     <div className="bg-gray-900">
       <Navbar />
       <Hero />
+      <Video/>
       <Club />
       <Events />
       <Card/>
