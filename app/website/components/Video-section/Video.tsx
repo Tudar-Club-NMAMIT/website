1061fec8e5f04f6048a86e34a4837de86a71a0ae
Htivus <suvithkumar0001@gmail.com>
2024-01-14 20:22:15 +0530

    video comp

--- app/website/components/Video-section/Video.tsx
+++ app/website/components/Video-section/Video.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Video = () => {
   return (
-    <div className=" ">
-      <video autoPlay muted loop className='w-screen h-screen'>
+    <div className="relative h-screen md:hidden">
+      <video autoPlay muted loop className='w-full h-full object-cover'>
         <source src="src/fire.mp4" />
       </video>
+      <div className="absolute inset-0 gradient-overlay"></div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <p className="text-black text-6xl font-malayalam font-bold">നിനക്ക് </p>
+      </div>
     </div>
   );
 }
 
-export default Video
+export default Video;
