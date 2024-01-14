f515ad025d7200a0cfab897162e8ec09dc10c263
Htivus <suvithkumar0001@gmail.com>
2024-01-14 21:12:51 +0530

    Added linear fadeout for hero bottom

--- app/website/components/Hero/Hero.tsx
+++ app/website/components/Hero/Hero.tsx
@@ -1,42 +1,49 @@
-"use client"
 import React, { useRef } from "react";
 import Image from "next/image";
-import "./Hero.css"
+import "./Hero.css";
 import { motion, useScroll, useTransform } from "framer-motion";
+
 const Hero = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
+
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   const textY = useTransform(scrollYProgress, [0, 1], ["-70%", "150%"]);
+
   return (
     <div
       ref={ref}
-      className="w-full h-screen overflow-hidden relative grid place-items-center">
+      className="w-full h-screen overflow-hidden relative grid place-items-center"
+    >
       <motion.div
         style={{ y: textY }}
-        className="opacity-100 font-bold relative z-30">
+        className="opacity-100 font-bold relative z-30"
+      >
         <Image
           src="/src/tudar.png"
           alt="Tudar"
           width={650}
-          height={300}></Image>
+          height={300}
+        ></Image>
       </motion.div>
+
       <motion.div
         className="min-h-screen bg-contain object-center absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(/src/p-below-img.png)`,
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2) 100px), url(/src/p-below-img.png)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
           y: backgroundY,
         }}
       />
+
       <div
         className="absolute inset-0 z-20 "
         style={{
-          backgroundImage: `url(/src/p-above-img.png)`,
+          backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0) 100px ), url(/src/p-above-img.png)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
         }}
@@ -46,4 +53,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
