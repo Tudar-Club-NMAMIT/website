53e4049b1d66d18a227d16339ae722694009fbe7
Htivus <suvithkumar0001@gmail.com>
2024-01-14 19:21:17 +0530

    changed nav ref in blog

--- app/Blogs/page.tsx
+++ app/Blogs/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { useSession } from 'next-auth/react'
-import Navbar from '../game/components/Navbar';
+import Navbar from '../website/components/Navbar/Navbar';
 const page = () => {
     const {data:session} = useSession();
   return (
