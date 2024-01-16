0006fb7c02a13ad5b0b196414ed4fdcb10d1091a
Amith-Jagannath <amithjagannath02@gmail.com>
2024-01-16 14:45:51 +0530

    added blogs backend

--- app/server/actions.ts
+++ app/server/actions.ts
@@ -2,17 +2,31 @@
 
 import { prisma } from "../utils/db";
 
-export async function createPost(title: string, desc: string) {
+export async function createPost(title: string, desc: string, image: string) {
   const user = await prisma.user.findUnique({
     where: {
-      email: "amithjagannath02@gmail.com",
+      email: "nittetudar@gmail.com",
     },
   });
   await prisma.post.create({
     data: {
       title: title,
       content: desc,
+      imageUrl: image,
       author: { connect: { id: user?.id } },
     },
   });
+}
+
+export async function getPosts() {
+  const posts = await prisma.user.findMany({
+    where: {
+      email: "nittetudar@gmail.com",
+    },
+    include: {
+      posts: true,
+    },
+  });
+
+  return posts;
 }
