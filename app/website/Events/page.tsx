9fef410ad4fdf805089effb329ce9ee1ca4e1cc9
Amith-Jagannath <amithjagannath02@gmail.com>
2024-01-18 14:58:08 +0530

    events

--- app/website/Events/page.tsx
+++ app/website/Events/page.tsx
@@ -1,115 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { getAllEvents, getPosts } from "@/app/server/actions";
+
 import Image from "next/image";
+import Link from "next/link";
 
-function Events() {
-  const posts = [
-    {
-      id: 1,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 2,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 3,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 4,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 5,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-    {
-      id: 6,
-      title: "Boost your conversion rate",
-      href: "#",
-      img: "/src/sample.jpg",
-      description:
-        "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-      date: "Mar 16, 2020",
-      datetime: "2020-03-16",
-      author: {
-        name: "Michael Foster",
-        role: "Co-Founder / CTO",
-        href: "#",
-        imageUrl:
-          "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-    },
-
-    // More posts...
-  ];
+const page = async () => {
+  const events = await getAllEvents();
+  
+  console.log("pots:" + events);
   return (
     <div>
       <Navbar />
@@ -123,58 +23,56 @@ function Events() {
               Where Tradition Meets the Digital Age
             </p>
           </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
+           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {events.map((event) => (
+            
               <article
-                key={post.id}
-                className="flex max-w-xl flex-col items-start justify-between p-6">
+                key={event.id}
+                className="flex max-w-xl flex-col items-start justify-between p-6"
+              >
                 <Image
                   className="rounded-md"
-                  src={post.img}
+                  src={event.imageUrl}
                   alt=""
                   width={500}
                   height={500}
                 />
-                <div className="flex items-center p-6 gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-white">
-                    {post.date}
-                  </time>
-                </div>
+                
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-white group-hover:text-white">
-                    <a href={post.href}>
+                    <a href="">
                       <span className="absolute inset-0" />
-                      {post.title}
+                      {event.title}
                     </a>
                   </h3>
                   <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">
-                    {post.description}
+                    {event.description}
                   </p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
                   <img
-                    src={post.author.imageUrl}
+                    src="https://lh3.googleusercontent.com/a/ACg8ocJB_jXg-txmKOlPMmgamsf5qUgExNPG6gm2tofOXCPR=s96-c"
                     alt=""
                     className="h-10 w-10 rounded-full bg-gray-50"
                   />
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-white">
-                      <a href={post.author.href}>
+                      <a href="">
                         <span className="absolute inset-0" />
-                        {post.author.name}
+                        Tudar Nitte
                       </a>
                     </p>
-                    <p className="text-white">{post.author.role}</p>
-                  </div>
+                    {/* <p className="text-white">{post.author.role}</p> */}
+                   </div>
                 </div>
               </article>
             ))}
-          </div>
+          </div> 
         </div>
       </div>
       <Footer />
     </div>
   );
-}
+};
 
-export default Events;
+export default page;
