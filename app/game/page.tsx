93db864e4a2bd8c5cffc3538572307dae5ff3ae9
Adarshhetty <adarshetty18@gmail.com>
2024-01-19 16:30:27 +0530

    UI chnages

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -7,10 +7,10 @@ export default function BackgroundBeamsDemo() {
     <div className="h-[40rem] font-sans w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  mb-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          ತುಡರ್
+          TUDAR
         </h1>
         <h3 className="relative z-10 text-lg md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          ತುಳು ಆಟ ಬೇಗೊಡು ಬರ್ರೆಗುಂಡು
+          Tulu quiz coming soon...!
         </h3>
       </div>
       <BackgroundBeams />
