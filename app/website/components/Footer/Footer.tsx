051c8c83fd347c16123c5c772a29eb54c4aebd9a
Adarshhetty <adarshetty18@gmail.com>
2024-01-21 15:46:51 +0530

    removed dark and light mode dependency

--- app/website/components/Footer/Footer.tsx
+++ app/website/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <section className="bg-[#101010] rounded-lg">
+    <section className="bg-[#101010]">
       <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <div className="px-5 py-2">
