d2593d17546311044c922e2bc1d79b438d042bab
Htivus <suvithkumar0001@gmail.com>
2024-02-04 14:13:04 +0530

    changed mayur desc

--- app/website/Aboutus/Team.tsx
+++ app/website/Aboutus/Team.tsx
@@ -558,7 +558,7 @@ export const Team = () => {
                 <p className="text-lg font-bold">Mayur Shet</p>
                 <p className="mb-4 text-xs text-gray-100">co head SMC</p>
                 <p className="mb-4 text-sm tracking-wide text-gray-100">
-                  Undu dada padun marey 😭
+                Creative visionary curating compelling visual stories for social media excellence.
                 </p>
               </div>
             </div>
