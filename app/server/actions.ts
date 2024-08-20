46869ebab06248be6816458bb008261ed889653b
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-08-21 00:11:55 +0530

    [feat] added createMemberProfile action

--- app/server/actions.ts
+++ app/server/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { Role } from "@prisma/client";
 import { prisma } from "../utils/db";
 
 export async function createPost(
@@ -94,12 +95,12 @@ export async function getEventById(id: string) {
   return event;
 }
 
-export async function getUsers(){
+export async function getUsers() {
   const users = await prisma.user.findMany();
   return users;
 }
 
-export async function getEvents(){
+export async function getEvents() {
   const events = await prisma.events.findMany();
   return events;
 }
@@ -122,48 +123,69 @@ export async function getAllEvents() {
   return events;
 }
 
-export async function updateUserfromDashboard(id:string, name:string, email:string, role:string, member:boolean){
+export async function updateUserfromDashboard(
+  id: string,
+  name: string,
+  email: string,
+  role: Role,
+  member: boolean
+) {
   const updatequery = await prisma.user.update({
-    where:{
-        id: id,
-    },
-    data:{
-        name: name,
-        role: role,
-        email: email,
-        isMember: member
-    
-    },
-})
-}
-
-export async function updateBlogfromDashboard(id:string, title:string, createdBy:string, show: boolean){
-  const updatequery = await prisma.post.update({
-    where:{
+    where: {
       id: id,
     },
-    data:{
+    data: {
+      name: name,
+      role: role,
+      email: email,
+      isMember: member,
+    },
+  });
+}
+
+export async function updateBlogfromDashboard(
+  id: string,
+  title: string,
+  createdBy: string,
+  show: boolean
+) {
+  const updatequery = await prisma.post.update({
+    where: {
+      id: id,
+    },
+    data: {
       title: title,
       createdBy: createdBy,
       show: show,
-    }
-  })
+    },
+  });
 }
 
-export async function updateEventsfromDashboard(id:string, title:string, attendedBy: string, venue: string, date:string){
+export async function updateEventsfromDashboard(
+  id: string,
+  title: string,
+  attendedBy: string,
+  venue: string,
+  date: string
+) {
   const updatequery = await prisma.events.update({
-    where:{
+    where: {
       id: id,
     },
-    data:{
-        title: title,
-        attendedBy: attendedBy,
-        venue: venue,
-        date: date,
-    }
-  })
+    data: {
+      title: title,
+      attendedBy: attendedBy,
+      venue: venue,
+      date: date,
+    },
+  });
 }
-export async function updateUserProfile(image:string, name:string, bio:string,email:string) {
+export async function updateUserProfile(
+  image: string,
+  name: string,
+  bio: string,
+  email: string
+) {
   const res = await prisma.user.update({
     where: {
       email: email,
@@ -176,79 +198,107 @@ export async function updateUserProfile(image:string, name:string, bio:string,em
   });
   return res;
 }
-export const addScoreToUser = async (userId: string, isRight: boolean, points: number) => {
+
+export async function createMemberProfile( //TODO: Add branch field and see whether to remove name field
+  email: string,
+  name: string,
+  usn: string,
+  year: number,
+  phone: string
+) {
+  const res = await prisma.user.update({
+    where: {
+      email: email,
+    },
+    data: {
+      name: name,
+      usn: usn,
+      year: year,
+      phoneNumber: phone,
+      role: Role.MEMBER,
+      isMember: true,
+    },
+  });
+  return res;
+}
+export const addScoreToUser = async (
+  userId: string,
+  isRight: boolean,
+  points: number
+) => {
   const perform = await prisma.user.findUnique({
     where: {
-      id: userId
+      id: userId,
     },
     select: {
-      perform: true
-    }
-  })
-  console.log(perform)
-  const performId = perform?.perform?.id
+      perform: true,
+    },
+  });
+  console.log(perform);
+  const performId = perform?.perform?.id;
   var updated;
   if (isRight) {
     updated = await prisma.perform.update({
       where: {
-        id: performId
+        id: performId,
       },
       data: {
         streak: {
-          increment: 1
+          increment: 1,
         },
         score: {
-          increment: points
+          increment: points,
         },
-      }
-    })
+      },
+    });
   } else {
     updated = await prisma.perform.update({
       where: {
-        id: performId
-      }, 
+        id: performId,
+      },
       data: {
-        streak: 0
-      }
-    })
+        streak: 0,
+      },
+    });
   }
   return updated;
-}
+};
 export const getPerform = async (userId: string) => {
-  const perform = await prisma.perform.findUnique({where: {
-    userId: userId
-  }})
+  const perform = await prisma.perform.findUnique({
+    where: {
+      userId: userId,
+    },
+  });
   if (perform) {
-    return perform
+    return perform;
   } else {
     const newperform = await prisma.perform.create({
       data: {
         userId: userId,
         score: 0,
         streak: 0,
-      }
-      
-    })
+      },
+    });
     const updateuser = await prisma.user.update({
       where: {
-        id: userId
+        id: userId,
       },
       data: {
         perform: {
           connect: {
-            id: newperform.id
-          }
-        }
-      }
-    })
-    return newperform
+            id: newperform.id,
+          },
+        },
+      },
+    });
+    return newperform;
   }
-}
+};
 export const getUserId = async (email: string) => {
   const user = await prisma.user.findUnique({
     where: {
-      email: email
-    }
-  })
-  return user?.id
-}
+      email: email,
+    },
+  });
+  return user?.id;
+};
