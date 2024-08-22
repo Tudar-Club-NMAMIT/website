3ab0099287ff6578f7062f1649332578fb125a26
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-08-23 01:20:39 +0530

    [style] changed signout button component background color to red

--- app/website/components/SignOut.tsx
+++ app/website/components/SignOut.tsx
@@ -1,15 +1,16 @@
-"use client"
-import { signOut } from 'next-auth/react';
-import React from 'react'
+"use client";
+import { signOut } from "next-auth/react";
+import React from "react";
 
 const SignOut = () => {
   return (
     <button
       onClick={() => signOut()}
-      className="block text-black focus:ring-4 focus:outline-none bg-white border font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+      className="block focus:ring-4 focus:outline-none bg-red-700 border-2 border-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+    >
       Sign Out
     </button>
   );
-}
+};
 
-export default SignOut
+export default SignOut;
