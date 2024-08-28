762c5f4814a7f3668d3f424012d625611c5177ed
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-08-28 21:26:49 +0530

    [refactor] changed admin email to tudar@nmamit.in

--- app/website/Blogs/page.tsx
+++ app/website/Blogs/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const page = async () => {
   const userData = await getPosts();
-  const posts = userData[0].posts;
+  const posts = userData[0]?.posts || null;
   return (
     <div>
       <div className="bg-black text-justify font-sans py-24 sm:py-32">
@@ -16,7 +16,7 @@ const page = async () => {
             </h2>
           </div>
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
+            {posts?.map((post) => (
               <Link key={post.id} href={`/Blog/${post.id}`}>
                 <Image
                   className="rounded-md"
