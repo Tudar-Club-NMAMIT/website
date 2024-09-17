498d77a7f6622d660ac1f109dedd49219f6d371a
Amrith R Naik <amrithrnaik@gmail.com>
2024-09-17 23:24:12 +0530

    [style] changed landing page bg

--- app/website/components/Hero/Hero.tsx
+++ app/website/components/Hero/Hero.tsx
@@ -20,28 +20,35 @@ const Hero = () => {
       <motion.div
         style={{ y: textY }}
         className="opacity-100 font-bold relative z-30">
+           {/* <div 
+          style={{
+          backgroundImage: `radial-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 500px)`
+          }} className="w-[200%] h-[200%] absolute -translate-x-1/4 -translate-y-1/4 z-40"></div> */}
         <Image
+        className="z-50"
           src="/src/tudar-text-1.png"
           alt="Tudar"
           width={420}
           height={250}></Image>
+         
       </motion.div>
 
-      <motion.div
+      {/* <motion.div
         className="min-h-screen bg-contain object-center absolute inset-0 z-0"
         style={{
+          // backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2) 100px), url(https://res.cloudinary.com/deax1ssmv/image/upload/f_auto,q_auto/v1/Tudar/h67xebdfxglmsx3l8f8i)`,
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2) 100px), url(https://res.cloudinary.com/deax1ssmv/image/upload/f_auto,q_auto/v1/Tudar/h67xebdfxglmsx3l8f8i)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
           y: backgroundY,
         }}
-      />
+      /> */}
 
       <div
-        className="absolute inset-0 z-20 "
+        className="absolute inset-0 z-20 opacity-75"
         style={{
-          backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 100px ), url(https://res.cloudinary.com/deax1ssmv/image/upload/f_auto,q_auto/v1/Tudar/cujh3cq4rorvbibsdbgl)`,
-          backgroundPosition: "bottom",
+          backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0) 300px ), url(https://res.cloudinary.com/dixgnlrzf/image/upload/v1726585567/Kambala-Karnataka1_cbokxs.jpg)`,
+          backgroundPosition: "center",
           backgroundSize: "cover",
         }}
       />
